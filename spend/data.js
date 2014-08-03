6e517dad540992336a4f9e792b9177338af5106b
HartDC <alma@alma.default>
2014-08-04 00:41:13 +0100

    spend data minify refactor part 1

--- spend/data.js
+++ spend/data.js
@@ -1,3 +1,8 @@
+var dFY = ["2010/11","2011/12","2012/13","2013/14"];
+var dEA = ["EA1","EA2","EA3","EA4"];
+var dET = ["ET1","ET2","ET3","ET4"];
+var dS = ["S1","S2","S3","S4"];
+
 var dataset = [
-{"ID":0,"JD":"2010-04-01","UD":1270080000,"FY":"10/11","ET":"Grants & Loans","EA":"Parish","S":"REDACTED","V":111.65},
-{"ID":1,"JD":"2014-03-27","UD":1395878400,"FY":"13/14","ET":"Leisure Activity","EA":"Leisure","S":"REDACTED","V":111.24}];
+{ID:0,UD:12700800,FY:0,ET:0,EA:0,S:0,V:111.65},
+{ID:1,UD:13958784,FY:4,ET:1,EA:0,S:2,V:111.24}];
