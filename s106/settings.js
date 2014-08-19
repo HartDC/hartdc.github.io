7b5bbee9993bff550fbb0fa19c036324e45a3909
HartDC <alma@alma.default>
2014-08-19 23:58:37 +0100

    s106 update
    still lots todo but mapping feature in place

---
+++ s106/settings.js
@@ -0,0 +1,27 @@
+
+/*Parishes - If the parish collects contributions set c: to 1 if not set it to 0 */
+
+var parishes = [
+{p:'Blackwater and Hawley',c:0},
+{p:'Bramshill',c:1},
+{p:'Church Crookham',c:0},
+{p:'Crondall',c:1},
+{p:'Crookham Village',c:0},
+{p:'Dogmersfield',c:1},
+{p:'Elvetham Heath',c:0},
+{p:'Eversley',c:1},
+{p:'Ewshot',c:0},
+{p:'Fleet',c:1},
+{p:'Greywell',c:0},
+{p:'Hartley Wintney',c:1},
+{p:'Heckfield',c:0},
+{p:'Hook',c:1},
+{p:'Long Sutton',c:0},
+{p:'Mattingley',c:1},
+{p:'Odiham',c:0},
+{p:'Rotherwick',c:1},
+{p:'South Warnborough',c:0},
+{p:'Winchfield',c:1},
+{p:'Yateley',c:0}
+];
+      
