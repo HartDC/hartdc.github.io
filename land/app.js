1be822bf42c8e5d2fcb41cd0ab2ca3039c20186a
HartDC <HartDC@users.noreply.github.com>
2014-08-22 16:53:46 +0100

    Update app.js

--- land/app.js
+++ land/app.js
@@ -1,23 +1,19 @@
 (function () {
-var app = angular.module('app', ['leaflet-directive']);
-
+var app = angular.module('app', ['leaflet-directive','ui.bootstrap']);
 
 app.controller("appCtrl", [ '$scope', '$timeout', function($scope, $timeout) {
-
- $scope.$on("leafletDirectiveMap.geojsonClick", function(ev, featureSelected, leafletEvent) {
+	
+	$scope.$on("leafletDirectiveMap.geojsonClick", function(ev, featureSelected, leafletEvent) {
                 Click(featureSelected, leafletEvent);            });
     
-var col = {color: '#222',fillColor: '#222',opacity: 1.0,fillOpacity: 0.4,weight: 1,smoothFactor: 1.0};
-    
-  angular.extend($scope, { 
+	var col = {color: '#F37B1D',fillColor: '#F37B1D',opacity: 1.0,fillOpacity: 0.4,weight: 1,smoothFactor: 1.0};
+  	angular.extend($scope, { 
         center: { lat: 51.275592, lng: -0.892855,  zoom: 11     },
 		layers: { baselayers: {
                     osm: {  
                         name: 'OpenStreetMap',
-                        url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                 
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-                        type: 'xyz'   }            }        },
+                        url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',                
+                        type: 'xyz'  } } },
 
 		geojson: {  data: land,    style: col      },
  		markers: {
@@ -31,19 +27,14 @@ var col = {color: '#222',fillColor: '#222',opacity: 1.0,fillOpacity: 0.4,weight:
 	     
 			});
 
-  function Click(Land, event) {
-		    $scope.choice = Land.meta.name;
+  	function Click(Land, event) {
+		    $scope.choice = Land.meta.owner;
+		    $scope.url = Land.meta.url;
 		    $scope.markers.point.lat = event.latlng.lat;
 	  	    $scope.markers.point.lng = event.latlng.lng;
 		  }
 
-
-
- 		$scope.Math = window.Math;
-
-
-
-
+      $scope.postcodes = pcodes;
 }]);
 
 })();
