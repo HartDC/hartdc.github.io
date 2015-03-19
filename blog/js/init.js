99cfc9b1f072a644c39bd022d0872728b4c370f9
HartDC <ashley.grist@hart.gov.uk>
2015-03-19 10:18:08 +0000

    Design Council ppt
    materialize test / ppt

---
+++ blog/js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav({closeOnClick: true});
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
