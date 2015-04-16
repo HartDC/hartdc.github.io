1faaf5df2a756c073904a584eee5da67f7b9a868
HartDC <ashley.grist@hart.gov.uk>
2015-04-16 07:16:12 +0100

    update project

--- phones/project.js
+++ phones/project.js
@@ -4,22 +4,7 @@ angular.module('project', ['ngRoute', 'firebase'])
 .service('fbRef', function(fbURL) {
   return new Firebase(fbURL)
 })
-.service('fbAuth', function($q, $firebase, $firebaseAuth, fbRef) {
-  var auth;
-  return function () {
-      if (auth) return $q.when(auth);
-      var authObj = $firebaseAuth(fbRef);
-      if (authObj.$getAuth()) {
-        return $q.when(auth = authObj.$getAuth());
-      }
-      var deferred = $q.defer();
-      authObj.$authAnonymously().then(function(authData) {
-          auth = authData;
-          deferred.resolve(authData);
-      });
-      return deferred.promise;
-  }
-})
+
  
 .service('Projects', function($q, $firebase, fbRef, fbAuth) {
   var self = this;
