f8a55c18e52c99cc59fc9857e83b8a97caef2cd4
HartDC <ashley.grist@hart.gov.uk>
2015-08-25 17:54:12 +0100

    create GW service framework
    initial load of a framework to prototype the garden waste service

---
+++ waste/app.js
@@ -0,0 +1,13 @@
+var app = angular.module('StarterApp', ['ngMaterial'])
+.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('teal')
+    .accentPalette('grey');
+});
+
+app.controller('AppCtrl', ['$scope', function($scope){
+  var newTabIndex = 0;
+$scope.selectedIndex = newTabIndex;
+
+
+}]);
