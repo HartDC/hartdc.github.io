d24a278071b6326742ab8076ee8648e858752231
HartDC <ashley.grist@hart.gov.uk>
2017-04-05 19:45:34 +0100

    Refresh to https

--- blog/js/init.js
+++
@@ -1,8 +0,0 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav({closeOnClick: true});
-    $('.parallax').parallax();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
